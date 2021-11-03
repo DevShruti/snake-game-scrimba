a9cf69d65fc1cf6cd2d287f5ca5a0a5e0c8c5c9b
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-11-03 10:04:56 -0500

    snake showith scrimba index.js code

--- index.js
+++ index.js
@@ -1,23 +1,22 @@
-const grid = document.querySelector('.grid');
+const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
-const scoreDisplay = document.querySelector('#score');
+const score = document.getElementById('score')
 let squares = []
 let currentSnake = [0,1,2]
 
 function createGrid() {
-
-for (let i = 0 ; i < 100 ; i++){
-let square = document.createElement('div');
-
-
-
-  square.classList.add('square');
-
-grid.appendChild(square);
-
-squares.push('square')
-} }
-
+    //create 100 of these elements with a for loop
+    for (let i=0; i < 100; i++) {
+     //create element
+    const square = document.createElement('div')
+    //add styling to the element
+    square.classList.add('square')
+    //put the element into our grid
+    grid.appendChild(square)
+    //push it into a new squares array    
+    squares.push(square)
+    }
+}
 createGrid()
 
 currentSnake.forEach(index => squares[index].classList.add('snake'))
