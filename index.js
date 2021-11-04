975d0cafdbe4f1104b6cb4551777f6416496fef6
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-11-04 09:08:56 -0500

    hitting the wall concept.

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const score = document.getElementById('score')
 let squares = []
 let currentSnake = [2,1,0]
 let direction = 1
+let width = 10
 
 function createGrid() {
     //create 100 of these elements with a for loop
@@ -23,6 +24,14 @@ createGrid()
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function move() {
+    if (
+        (currentSnake[0] + width >= 100 && direction === 10) ||
+        (currentSnake[0] - width < 0 &&  direction === -10) ||
+        (currentSnake[0] % width === 9 &&  direction === 1)  ||
+        (currentSnake[0] % width === 0 && direction === -1 ) ||
+        squares[currentSnake[0] + direction].classList.contains('snake')
+
+    ) return clearInterval(timerId)
     
     const tail = currentSnake.pop()
       
@@ -46,15 +55,16 @@ let timerId = setInterval(move, 1000);
 
 function control(e) { 
     if (e.key === 'ArrowRight') {
+        direction = 1
 
 } else if (e.key === 'ArrowUp') {
-    console.log('up pressed')
+        direction = -width
 
 } else if (e.key === 'ArrowLeft') {
-    console.log('left pressed')
+       direction = -1
 
 } else if (e.key === 'ArrowDown') {
-    console.log('down pressed')
+      direction = +width
 
 }
 } 
