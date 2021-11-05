bde668a98cda4a8e8dd4e062ee3ad2cf14412598
Shruti Malik <shruti.malik.ramaswamy@gmail.com>
2021-11-05 05:51:07 -0500

    snake game working

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 A simple snake game with HTML, CSS and Javascript
 
 ![Javascript methods learnt](/images/Screenshot.png)
+
+![snake game video](/snakegame.mp4)
--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const grid = document.querySelector('.grid')
 const startButton = document.getElementById('start')
-const score = document.getElementById('score')
+const scoreDisplay = document.getElementById('score')
 let squares = []
 let currentSnake = [2,1,0]
 let direction = 1
-let width = 10
+const width = 10
+let appleIndex = 0
+let score = 0
+let intervalTime = 1000
+let speed = .9
+let timerId = 0
 
 function createGrid() {
     //create 100 of these elements with a for loop
-    for (let i=0; i < 100; i++) {
+    for (let i=0; i < width*width; i++) {
      //create element
     const square = document.createElement('div')
     //add styling to the element
@@ -21,37 +26,75 @@ function createGrid() {
 }
 createGrid()
 
+function startGame() {
+   
+    currentSnake.forEach(index => squares[index].classList.remove('snake'))
+    squares[appleIndex].classList.remove('apple')
+   clearInterval(timerId)
+   currentSnake = [2,1,0]
+   score = 0
+   scoreDisplay.textContent= score
+   intervalTime = 1000
+   direction = 1
+   generateApple()
+   currentSnake.forEach(index => squares[index].classList.add('snake'))
+   timerId = setInterval(move, intervalTime)
+}
+
 currentSnake.forEach(index => squares[index].classList.add('snake'))
 
 function move() {
     if (
-        (currentSnake[0] + width >= 100 && direction === 10) ||
-        (currentSnake[0] - width < 0 &&  direction === -10) ||
-        (currentSnake[0] % width === 9 &&  direction === 1)  ||
-        (currentSnake[0] % width === 0 && direction === -1 ) ||
+        (currentSnake[0] + width >= width*width && direction === width) || //if snake has hit bottom
+        (currentSnake[0] % width === width-1 && direction === 1) || //if snake has hit right wall
+        (currentSnake[0] % width === 0 && direction === -1) || //if snake has hit left wall
+        (currentSnake[0] - width < 0 && direction === -width) || //if snake has hit top
         squares[currentSnake[0] + direction].classList.contains('snake')
+    )
+    return clearInterval(timerId)
 
-    ) return clearInterval(timerId)
-    
+    //remove last element from our currentSnake array
     const tail = currentSnake.pop()
-      
+    //remove styling from last element
     squares[tail].classList.remove('snake')
-
+    //add square in direction we are heading
     currentSnake.unshift(currentSnake[0] + direction)
-
-   squares[ currentSnake[0]].classList.add('snake')
-
-
-
-     
-   
-
-   
+    //add styling so we can see it
+    
+    //deal with snake head gets apple
+    if (squares[currentSnake[0]].classList.contains('apple')) {
+        //remove the class of apple
+        squares[currentSnake[0]].classList.remove('apple')
+        //grow our snake by adding class of snake to it
+        squares[tail].class //grow our snake array
+        currentSnake.push(tail)
+        //generate new apple
+        generateApple()
+        //add one to the score
+        score++
+         //display our score
+        scoreDisplay.textContent = score
+        //speed up our snake
+        clearInterval(timerId)
+        intervalTime = intervalTime * speed
+        timerId = setInterval(move, intervalTime)
+        
+    }
+    
+    
+    
+    squares[currentSnake[0]].classList.add('snake')
 }
 
-move()
 
-let timerId = setInterval(move, 1000);
+function generateApple() {
+    do {
+        appleIndex = Math.floor(Math.random() * squares.length)
+    } while (squares[appleIndex].classList.contains('snake'))
+    squares[appleIndex].classList.add('apple')
+} 
+generateApple()
+
 
 function control(e) { 
     if (e.key === 'ArrowRight') {
@@ -70,3 +113,4 @@ function control(e) {
 } 
 
 document.addEventListener('keyup', control)
+document.addEventListener('click', startGame)
